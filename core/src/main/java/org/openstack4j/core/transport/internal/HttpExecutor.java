5a232ca44f2bb724f80f343b0b27a4cdf052a10b
zhimin <zhimin@fit2cloud.com>
2016-07-27 18:46:15 +0800

    fix bug

--- core/src/main/java/org/openstack4j/core/transport/internal/HttpExecutor.java
+++ core/src/main/java/org/openstack4j/core/transport/internal/HttpExecutor.java
@@ -47,7 +47,7 @@ public class HttpExecutor  {
      * Delegate to {@link HttpExecutorService#execute(HttpRequest)}
      */
     public <R> HttpResponse execute(HttpRequest<R> request) {
-        LOG.error("Executing Request: %s -> %s", request.getEndpoint(), request.getPath());
+        LOG.debug("Executing Request: %s -> %s", request.getEndpoint(), request.getPath());
         return service().execute(request);
     }
 }
